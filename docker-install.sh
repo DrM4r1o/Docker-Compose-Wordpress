86bdb54f1e0a0786a726e5cf6be37fde178d9f7a
m4r1oph <94112810+m4r1oph@users.noreply.github.com>
2023-05-04 21:16:01 +0200

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -6,6 +6,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce
+sudo systemctl status docker
 sudo usermod -aG docker ${USER}
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
